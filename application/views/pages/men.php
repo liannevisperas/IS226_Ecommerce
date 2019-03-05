62f20517561b6572540f1da89b92e99b802a35b6
Visperas <lianne.k.b.visperas@accenture.com>
2019-03-05 23:10:34 +0800

    Pushed ver 1

---
+++ application/views/pages/men.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>men page of the-shop</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>the-shop</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="application/libraries/my-style.css" type="text/css"/>
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>	
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"/>
+	</head>
+
+	<body style="background-color:#C5C1C0">
+	<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#0A1612;">
+  		<a class="navbar-brand" href="#">The Shop</a>
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+	  	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+	    	<div class="navbar-nav">
+	      		<a class="nav-item nav-link active" href="<?php echo base_url(); ?>women">Women<span class="sr-only">(current)</span></a>
+	      		<a class="nav-item nav-link" href="<?php echo base_url(); ?>men">Men</a>
+	      		<a class="nav-item nav-link" href="<?php echo base_url(); ?>kids">Kids</a>
+	      		<a class="nav-item nav-link" href="<?php echo base_url(); ?>accessories">Accessories</a>
+	      		<a class="nav-item nav-link" href="<?php echo base_url(); ?>signin" class="d-flex justify-content-end">Sign Up</a>
+	    	</div>
+	  	</div>
+	  	<form class="form-inline my-2 my-lg-0">
+      		<input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
+      		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    	</form>
+
+	</nav>
+
+
+	<div class="container-fluid">
